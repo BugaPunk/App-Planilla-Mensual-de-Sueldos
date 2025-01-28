fa989a6c50571b693d5596be33a6da9da666a2f9
BugaPunks <punksbuga@outlook.es>
2025-01-28 11:56:15 -0400

    [FIX] app se instala 2 vecess

--- app/src/main/java/com/bugabuga/planillamensualdesueldos/activities/LoginActivity.kt
+++ app/src/main/java/com/bugabuga/planillamensualdesueldos/activities/LoginActivity.kt
@@ -1,21 +1,24 @@
 package com.bugabuga.planillamensualdesueldos.activities
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.bugabuga.planillamensualdesueldos.R
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var btnRegister: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        btnRegister = findViewById(R.id.btnRegister)
+
+        btnRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
 }
